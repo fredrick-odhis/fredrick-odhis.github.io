85adfb35b59b209ee3d40f83f44916a025bfe0eb
Fredrick Odhiambo <freddieodhisb@gmail.com>
2018-09-26 09:56:09 +0300

    changes to link scroll and nav

--- js/myscript.js
+++ js/myscript.js
@@ -12,4 +12,28 @@ $(function() {
         }
  });
  
+ var scrollLink = $('.scroll');
+
+ // Smooth scrolling
+scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({ 
+        scrollTop: $(this.hash).offset().top
+    }, 1000)
+
+});
+
+// Active link switching
+$(window).scroll(function(){
+    var scrollbarLocation = $(this).scrollTop();
+    scrollLink.each(function() {
+        var sectionOffset = $(this.hash).offset().top-50;
+
+        if (sectionOffset <= scrollbarLocation) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+        }
+    });
+});
+
 });
